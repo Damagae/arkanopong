7015581b7f9ad7f1474460975bd26104f0972043
Damagae <daphnegm.rose@gmail.com>
2017-04-19 21:53:26 +0200

    Erreur de segmentation à cause du tableau level

--- dev/src/main.c
+++ dev/src/main.c
@@ -74,11 +74,8 @@ int main(int argc, char** argv)
   /** Creation des briques **/
   PtBrick brickList = NULL;
   int * level = loadLevel(levelpath);
-  createLevelBricks(level, GAME_WIDTH, GAME_HEIGHT, &brickList, &bonusList);
-  addBrick(&brickList, createBrick(PointXY(GAME_WIDTH/2 + (WINDOW_WIDTH-GAME_WIDTH)/2, GAME_HEIGHT/2 + (WINDOW_HEIGHT-GAME_HEIGHT)/2), BARDWN, &bonusList));
-  addBrick(&brickList, createBrick(PointXY(GAME_WIDTH/2 + (WINDOW_WIDTH-GAME_WIDTH)/2 - 200, GAME_HEIGHT/2 + (WINDOW_HEIGHT-GAME_HEIGHT)/2), BARUP, &bonusList));
-  addBrick(&brickList, createBrick(PointXY(GAME_WIDTH/2 + (WINDOW_WIDTH-GAME_WIDTH)/2 + 200, GAME_HEIGHT/2 + (WINDOW_HEIGHT-GAME_HEIGHT)/2), BARSPDUP, &bonusList));
-  addBrick(&brickList, createBrick(PointXY((WINDOW_WIDTH-GAME_WIDTH), GAME_HEIGHT/2 + (WINDOW_HEIGHT-GAME_HEIGHT)/2), ADDLIFE, &bonusList));
+  //printf("%d", level[0]);
+  //createLevelBricks(level, GAME_WIDTH, GAME_HEIGHT, &brickList, &bonusList);
 
   /** Initialisation de la SDL **/
   if(-1 == SDL_Init(SDL_INIT_VIDEO)) {
